c148147e716dc16208b6adc118dd1b8a8b5a5e8c
labinfo <labinfo@ectufrn.ect.ufrn.br>
2022-09-02 20:53:30 -0300

    erro

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      
       <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.texto}>ECT2525</Text>
+      <Image style={styles.imagem} source={require('./assets/imagens/ufrn.png')}></Image>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +20,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  texto :{
+    fontSize: 30,
+    fontWeight: 'bold',
+    fontFamily: 'monospace',
+    color: "#F3A545"
+  },
+  imagem : {
+    height: 200,
+    resizeMode: 'contain'
+  }
 });
